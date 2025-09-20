338e1653a60ba82edba25a5279865d9f90538bba
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-09-20 02:18:42 +0200

    Delete TestMixin.java

--- src/main/java/net/tclproject/entityculling/mixins/client/TestMixin.java
+++
@@ -1,9 +0,0 @@
-package net.tclproject.entityculling.mixins.client;
-
-import net.minecraft.client.renderer.EntityRenderer;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(EntityRenderer.class)
-public class TestMixin {
-}
