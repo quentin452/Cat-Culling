eba8f16a38a6b6a89d57866ce273ce09f3ad6d98
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-09-20 04:30:47 +0200

    Format files

--- src/main/java/fr/iamacat/catculling/CatCullingBase.java
+++ src/main/java/fr/iamacat/catculling/CatCullingBase.java
@@ -1,5 +1,10 @@
 package fr.iamacat.catculling;
 
+import net.minecraftforge.common.MinecraftForge;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
@@ -9,55 +14,57 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import fr.iamacat.catculling.handlers.Config;
-import net.minecraftforge.common.MinecraftForge;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-@Mod(modid = CatCullingBase.MODID, version = CatCullingBase.VERSION,
-     name = CatCullingBase.NAME, acceptableRemoteVersions = "*")
+@Mod(
+    modid = CatCullingBase.MODID,
+    version = CatCullingBase.VERSION,
+    name = CatCullingBase.NAME,
+    acceptableRemoteVersions = "*")
 public class CatCullingBase extends EntityCullingBase {
 
-  public static final String MODID = "catculling";
-  public static final String NAME = "Cat Culling Unofficial";
-  public static final String VERSION = "1.0.1";
+    public static final String MODID = "catculling";
+    public static final String NAME = "Cat Culling Unofficial";
+    public static final String VERSION = "1.0.1";
 
-  @Mod.Instance("catculling") public static CatCullingBase instance;
+    @Mod.Instance("catculling")
+    public static CatCullingBase instance;
 
-  private boolean onServer = false;
+    private boolean onServer = false;
 
-  public static final Logger logger = LogManager.getLogger("CatCullingBase");
+    public static final Logger logger = LogManager.getLogger("CatCullingBase");
 
-  @Override
-  public void initModloader() {}
+    @Override
+    public void initModloader() {}
 
-  @EventHandler
-  public void onPostInit(FMLPostInitializationEvent event) {
-    ClientRegistry.registerKeyBinding(keybind);
-    MinecraftForge.EVENT_BUS.register(this);
-    FMLCommonHandler.instance().bus().register(this);
-  }
-
-  @SubscribeEvent
-  public void doClientTick(TickEvent.ClientTickEvent event) {
-    this.clientTick();
-  }
-
-  @SubscribeEvent
-  public void doWorldTick(TickEvent.WorldTickEvent event) {
-    this.worldTick();
-  }
-
-  @EventHandler
-  public void preInit(FMLPreInitializationEvent event) {
-    Config.load(event);
-    try {
-      Class clientClass = net.minecraft.client.Minecraft.class;
-    } catch (Throwable ex) {
-      System.out.println(
-          "CatCullingBase Mod installed on a Server. Going to sleep.");
-      onServer = true;
-      return;
+    @EventHandler
+    public void onPostInit(FMLPostInitializationEvent event) {
+        ClientRegistry.registerKeyBinding(keybind);
+        MinecraftForge.EVENT_BUS.register(this);
+        FMLCommonHandler.instance()
+            .bus()
+            .register(this);
     }
-    onInitialize();
-  }
+
+    @SubscribeEvent
+    public void doClientTick(TickEvent.ClientTickEvent event) {
+        this.clientTick();
+    }
+
+    @SubscribeEvent
+    public void doWorldTick(TickEvent.WorldTickEvent event) {
+        this.worldTick();
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        Config.load(event);
+        try {
+            Class clientClass = net.minecraft.client.Minecraft.class;
+        } catch (Throwable ex) {
+            System.out.println("CatCullingBase Mod installed on a Server. Going to sleep.");
+            onServer = true;
+            return;
+        }
+        onInitialize();
+    }
 }
